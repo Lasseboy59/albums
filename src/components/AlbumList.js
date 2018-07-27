688f2479f922d9b65322a880df269f284b760913
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-07-27 15:45:59 +0300

    3 commit succeed to read dat from heroku app

---
+++ src/components/AlbumList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then((responseData) => {
+        // console.log('Hi there', responseData);
+        console.log(responseData);
+      });
+    }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
