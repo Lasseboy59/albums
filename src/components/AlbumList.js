68c0589799f1ea9b7f837e1b731e2e2e8c51fe96
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-07-27 16:55:53 +0300

    4th commit, writing the album title to the ios phone screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => response.json())
-      .then((responseData) => {
-        // console.log('Hi there', responseData);
-        console.log(responseData);
-      });
+      .then(response => this.setState({ albums: response }));
     }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
